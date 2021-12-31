8ea4f5d4541d427084b059c3d5b139579d2843e6
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2021-12-30 23:22:49 -0600

    update

--- answer.java
+++ answer.java
@@ -1,25 +1,34 @@
-public static void main(String args[]) {
-    public String mathFunction(int num1, int num2, int num3) {
-        String operation;
-        if (num1 + num2 == num3) {
-            operation.append("+");
-        } else if (num1 - num2 == num3) {
-            operation.append("-");
-        } else if (num1 * num2 == num3) {
-            operation.append("*");
-        } else if (num1 / num2 == num3) {
-            operation.append("/");
-        } else {
-            operation.append("none");
-        }
+public class Answer {
+
+    public static void main(String[] args) {
+
+        StringBuffer example1 = mathFunction(2, 2, 4);
+        System.out.println(example1);
+
     }
 
-    @Test
-    public void testMath(){
-        assertEquals("+", mathFunction(1,2,3));
-        assertEquals("+*", mathFunction(2,2,4));
+    public static StringBuffer mathFunction(int num1, int num2, int num3) {
+        StringBuffer x = new StringBuffer(" ");
+        if (num1 + num2 == num3 && num1 * num2 == num3) {
+            x.append("+*");
+        } else if (num1 + num2 == num3) {
+            x.append("+");
+        } else if (num1 - num2 == num3) {
+            x.append("-");
+        } else if (num1 * num2 == num3) {
+            x.append("*");
+        } else if (num1 / num2 == num3) {
+            x.append("/");
+        } else {
+            x.append("none");
+        }
+
+        return x;
+
     }
 }
 
-
-// Thanks again for giving me the chance to interview with you. I'll be honest my experties falls under javascript and react.js and this is my first time writing Java. I'm open to learning java and mastering any skill you will require of me to excel in my job.  I wish you the best and  Happy New Years. 
+// Thanks again for giving me the chance to interview with you. I'll be honest
+// my experties falls under javascript and react.js and this is my first time
+// writing Java. I'm open to learning java and mastering any skill you will
+// require of me to excel in my job. I wish you the best and Happy New Years.
